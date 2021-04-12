097461705f00d81eb7d1ae5ebcc47bbf75035a73
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-12 16:06:05 +0200

    Function

---
+++ Public/Connect-Wordpress.ps1
@@ -0,0 +1,30 @@
+﻿function Connect-Wordpress {
+    [cmdletBinding(DefaultParameterSetName = 'Password')]
+    param(
+        [Parameter(ParameterSetName = 'ClearText')]
+        [string] $UserName,
+        [Parameter(ParameterSetName = 'ClearText')]
+        [string] $Password,
+
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [alias('Uri')][Uri] $Url,
+        [Parameter(ParameterSetName = 'Password')]
+        [pscredential] $Credential
+    )
+    if ($Username -and $Password) {
+        $UserNameApi = $UserName
+        $PasswordApi = $Password
+    } elseif ($Credential) {
+        $UserNameApi = $Credential.UserName
+        $PasswordApi = $Credential.GetNetworkCredential().Password
+    }
+    $Authorization = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("$UserNameApi`:$PasswordApi")))
+    $Auth = @{
+        Header = @{
+            Authorization = -join ("Basic ", $Authorization)
+        }
+        Url    = "$Url/wp-json/wp/v2/"
+    }
+    $Auth
+}
