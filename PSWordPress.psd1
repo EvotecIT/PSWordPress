09e074b3dd5efde94b610d1bc26123036c1a7087
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-12 23:57:59 +0200

    Update

--- PSWordPress.psd1
+++ PSWordPress.psd1
@@ -5,15 +5,22 @@
     CompanyName          = 'Evotec'
     CompatiblePSEditions = @('Desktop', 'Core')
     Copyright            = '(c) 2011 - 2021 Przemyslaw Klys @ Evotec. All rights reserved.'
-    Description          = 'Module that allows interacting with WordPress.'
+    Description          = 'Module that allows interacting with WordPress via its Rest API'
     FunctionsToExport    = @('Connect-Wordpress', 'Get-WordPressCategory', 'Get-WordPressPage', 'Get-WordPressPost', 'Get-WordPressSetting', 'Get-WordPressTag', 'New-WordPressPost', 'Set-WordPressSetting')
     GUID                 = '412368da-4507-447c-8525-5cf7628b7ae6'
     ModuleVersion        = '0.0.1'
     PowerShellVersion    = '5.1'
     PrivateData          = @{
         PSData = @{
-            Tags = @('Windows', 'MacOS', 'Linux', 'Wordpress')
+            Tags       = @('Windows', 'MacOS', 'Linux', 'Wordpress')
+            ProjectUri = 'https://github.com/EvotecIT/PSWordPress'
+            IconUri    = 'https://evotec.xyz/wp-content/uploads/2021/04/WordPressLogo.png'
         }
     }
+    RequiredModules      = @(@{
+            ModuleVersion = '0.0.199'
+            ModuleName    = 'PSSharedGoods'
+            Guid          = 'ee272aa8-baaa-4edf-9f45-b6d6f7d844fe'
+        })
     RootModule           = 'PSWordPress.psm1'
 }
--- Public/New-WordPressPost.ps1
+++ Public/New-WordPressPost.ps1
@@ -1,6 +1,51 @@
 ﻿function New-WordPressPost {
-    [cmdletBinding()]
+    [cmdletBinding(SupportsShouldProcess)]
     param(
+        [System.Collections.IDictionary] $Authorization,
+        [string] $Date,
+        [string] $Slug,
+        [ValidateSet('publish', 'future', 'draft', 'pending', 'private')][string] $Status,
+        [string] $Title,
+        [string] $Password,
+        [string] $Content,
+        [int] $Author,
+        [string] $Excerpt,
+        [int] $FeaturedMedia,
+        [ValidateSet('open', 'closed')][string] $CommentStatus,
+        [ValidateSet('open', 'closed')][string] $PingStatus,
+        [ValidateSet('standard', 'aside', 'chat', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio')][string] $Format,
+        [string] $Meta,
+        [nullable[bool]] $Sticky,
+        [int[]] $Tags,
+        [int[]] $Categories
 
     )
+    $QueryParameters = [ordered] @{
+        title          = $Title
+        date           = $Date
+        slug           = $Slug
+        status         = $Status
+        password       = $Password
+        content        = $Content
+        excerpt        = $Excerpt
+        comment_status = $CommentStatus
+        ping_status    = $PingStatus
+        format         = $Format
+        meta           = $Meta
+    }
+
+    if ($Tags) {
+        $QueryParameters['tags'] = $Tags
+    }
+    if ($Categories) {
+        $QueryParameters['categories'] = $Categories
+    }
+    if ($FeaturedMedia) {
+        $QueryParameters['featured_media'] = $FeaturedMedia
+    }
+    if ($Author) {
+        $QueryParameters['author'] = $Author
+    }
+    Remove-EmptyValue -Hashtable $QueryParameters
+    Invoke-RestApi -PrimaryUri $Authorization.Url -Uri 'wp-json/wp/v2/posts' -QueryParameter $QueryParameters -Headers $Authorization.Header -Method POST
 }
