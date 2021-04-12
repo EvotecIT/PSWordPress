5b61117291b6814015404075ce9489b60143ec3a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-13 00:05:27 +0200

    Update

---
+++ Public/New-WordPressPage.ps1
@@ -0,0 +1,48 @@
+﻿function New-WordPressPage {
+    [cmdletBinding(SupportsShouldProcess)]
+    param(
+        [System.Collections.IDictionary] $Authorization,
+        [string] $Date,
+        [string] $Slug,
+        [ValidateSet('publish', 'future', 'draft', 'pending', 'private')][string] $Status,
+        [string] $Title,
+        [int] $Parent,
+        [string] $Password,
+        [string] $Content,
+        [int] $Author,
+        [string] $Excerpt,
+        [int] $FeaturedMedia,
+        [ValidateSet('open', 'closed')][string] $CommentStatus,
+        [ValidateSet('open', 'closed')][string] $PingStatus,
+        [string] $Meta,
+        [string] $Template,
+        [int] $MenuOrder
+    )
+    $QueryParameters = [ordered] @{
+        title          = $Title
+        date           = $Date
+        slug           = $Slug
+        status         = $Status
+        password       = $Password
+        content        = $Content
+        excerpt        = $Excerpt
+        comment_status = $CommentStatus
+        ping_status    = $PingStatus
+        meta           = $Meta
+        template       = $Template
+    }
+    if ($Parent) {
+        $QueryParameters['parent'] = $Parent
+    }
+    if ($FeaturedMedia) {
+        $QueryParameters['featured_media'] = $FeaturedMedia
+    }
+    if ($Author) {
+        $QueryParameters['author'] = $Author
+    }
+    if ($MenuOrder) {
+        $QueryParameters['menu_order'] = $MenuOrder
+    }
+    Remove-EmptyValue -Hashtable $QueryParameters
+    Invoke-RestApi -PrimaryUri $Authorization.Url -Uri 'wp-json/wp/v2/pages' -QueryParameter $QueryParameters -Headers $Authorization.Header -Method POST
+}
